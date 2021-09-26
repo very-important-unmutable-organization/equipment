b5cda6f5cc832a75766dc8bacc705e5a6c9fd3e1
Yury Frolov <yura200253@gmail.com>
2021-09-26 18:00:02 +0500

    Auto stash before rebase of "origin/architecture"

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"fmt"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/very-important-unmutable-organization/equipment/internal/domain"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"net/http"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	db, err := gorm.Open(postgres.Open("host=localhost"))
+	if err != nil {
+		panic(fmt.Sprintf("could not connect to database: %s", err))
+	}
+
+	r.Get("/equipment", func(w http.ResponseWriter, r *http.Request) {
+		equipments := new(domain.Equipment)
+		res := db.Find(equipments)
+		if res.Error != nil {
+			http.Error(w, http.StatusText(500), 500)
+			return
+		}
+		_, _ = w.Write([]byte(fmt.Sprint(equipments)))
+	})
+}
--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,10 @@
 module https://github.com/astaxie/build-web-application-with-golang
 
 go 1.17
+
+require (
+	github.com/go-chi/chi v1.5.4 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	gorm.io/driver/postgres v1.1.1 // indirect
+	gorm.io/gorm v1.21.15 // indirect
+)
