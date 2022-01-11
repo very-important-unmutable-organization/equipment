663322104a931fe49956745d4a5efc0cd2ac900f
Yury Frolov <yura200253@gmail.com>
2022-01-11 16:37:51 +0500

    Added JSONB type

--- src/internal/domain/equipment.go
+++ src/internal/domain/equipment.go
@@ -18,14 +18,14 @@ type Equipment struct {
 	Status          Status   `sql:"type:item_state"`
 	StateCode       int
 	State           State           `gorm:"foreignKey:StateCode"`
-	PurposeCode     int                    `sql:"not null"`
+	PurposeCode     int             `sql:"not null"`
 	Purpose         Purpose         `gorm:"foreignKey:PurposeCode"`
-	PurchaseDate    time.Time              `sql:"not null"`
-	Price           decimal.Decimal        `sql:"not null"`
-	Currency        currency               `sql:"type:currency;default:ruble"`
-	OriginCode      int                    `sql:"not null"`
+	PurchaseDate    time.Time       `sql:"not null"`
+	Price           decimal.Decimal `sql:"not null"`
+	Currency        currency        `sql:"type:currency;default:ruble"`
+	OriginCode      int             `sql:"not null"`
 	Origin          Origin          `gorm:"foreignKey:OriginCode"`
-	Characteristics map[string]interface{} `sql:"type:jsonb"`
-	Docs            []Document             `gorm:"foreignKey:ItemID"`
-	Photos          []Photo                `gorm:"foreignKey:ItemID"`
+	Characteristics JSONB           `sql:"type:jsonb"`
+	Docs            []Document      `gorm:"foreignKey:ItemID"`
+	Photos          []Photo         `gorm:"foreignKey:ItemID"`
 }
