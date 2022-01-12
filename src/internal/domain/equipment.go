c6556ae3c26fd49d91e13183c794429cc7cee60c
Yury Frolov <yura200253@gmail.com>
2022-01-12 15:38:36 +0500

    Some json field annotations

--- src/internal/domain/equipment.go
+++ src/internal/domain/equipment.go
@@ -13,18 +13,18 @@ type Equipment struct {
 	Category        Category `gorm:"type:category;not null"`
 	Name            string   `gorm:"type:varchar;not null"`
 	Description     string
-	SerialNumber    string   `gorm:"not null"`
-	TypeCode        int      `gorm:"not null"`
-	Type            ItemType `gorm:"foreignKey:TypeCode"`
-	Status          Status   `gorm:"type:status;not null"`
-	StateCode       null.Int
+	SerialNumber    string          `gorm:"not null" json:"serial_number"`
+	TypeCode        int             `gorm:"not null" json:"type_code"`
+	Type            ItemType        `gorm:"foreignKey:TypeCode"`
+	Status          Status          `gorm:"type:status;not null"`
+	StateCode       null.Int        `json:"state_code"`
 	State           State           `gorm:"foreignKey:StateCode"`
-	PurposeCode     int             `gorm:"not null"`
+	PurposeCode     int             `gorm:"not null" json:"purpose_code"`
 	Purpose         Purpose         `gorm:"foreignKey:PurposeCode"`
-	PurchaseDate    time.Time       `gorm:"not null"`
+	PurchaseDate    time.Time       `gorm:"not null" json:"purchase_date"`
 	Price           decimal.Decimal `gorm:"not null"`
 	Currency        currency        `gorm:"type:currency;default:ruble"`
-	OriginCode      int             `gorm:"not null"`
+	OriginCode      int             `gorm:"not null" json:"origin_code"`
 	Origin          Origin          `gorm:"foreignKey:OriginCode"`
 	Characteristics JSONB           `gorm:"type:jsonb"`
 	Docs            []Document      `gorm:"foreignKey:ItemID"`
